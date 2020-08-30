7a6a0fc9fd6e2d098c7c5f453de2c5b84cd952bd
Oliver Eilhard <oliver@eilhard.net>
2020-08-30 11:59:29 +0200

    Fix #1450 - Change in RangeQuery from, to, include_lower and include_upper parameters by gt, gte, lt, lte
    See https://www.elastic.co/guide/en/elasticsearch/reference/0.90/query-dsl-range-query.html
    
    I have keep From, To, IncludeLower and IncludeUpper for retro compatibility
    
    I don't know if you want some verification like these :
    1. at least one gt, gte, lt and lte
    2. not : gt + gte or lt + lte

--- search_queries_range_test.go
+++ search_queries_range_test.go
@@ -10,8 +10,12 @@ import (
 )
 
 func TestRangeQuery(t *testing.T) {
-	q := NewRangeQuery("postDate").From("2010-03-01").To("2010-04-01").Boost(3).Relation("within")
-	q = q.QueryName("my_query")
+	q := NewRangeQuery("postDate").
+		Gte("2010-03-01").
+		Lte("2010-04-01").
+		Format("yyyy-MM-dd").
+		Boost(3).
+		Relation(RelationWithin)
 	src, err := q.Source()
 	if err != nil {
 		t.Fatal(err)
@@ -21,7 +25,7 @@ func TestRangeQuery(t *testing.T) {
 		t.Fatalf("marshaling to JSON failed: %v", err)
 	}
 	got := string(data)
-	expected := `{"range":{"_name":"my_query","postDate":{"boost":3,"from":"2010-03-01","include_lower":true,"include_upper":true,"relation":"within","to":"2010-04-01"}}}`
+	expected := `{"range":{"postDate":{"boost":3,"format":"yyyy-MM-dd","gte":"2010-03-01","lte":"2010-04-01","relation":"WITHIN"}}}`
 	if got != expected {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
@@ -41,7 +45,7 @@ func TestRangeQueryWithTimeZone(t *testing.T) {
 		t.Fatalf("marshaling to JSON failed: %v", err)
 	}
 	got := string(data)
-	expected := `{"range":{"born":{"from":"2012-01-01","include_lower":true,"include_upper":true,"time_zone":"+1:00","to":"now"}}}`
+	expected := `{"range":{"born":{"gte":"2012-01-01","lte":"now","time_zone":"+1:00"}}}`
 	if got != expected {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
@@ -61,7 +65,69 @@ func TestRangeQueryWithFormat(t *testing.T) {
 		t.Fatalf("marshaling to JSON failed: %v", err)
 	}
 	got := string(data)
-	expected := `{"range":{"born":{"format":"yyyy/MM/dd","from":"2012/01/01","include_lower":true,"include_upper":true,"to":"now"}}}`
+	expected := `{"range":{"born":{"format":"yyyy/MM/dd","gte":"2012/01/01","lte":"now"}}}`
+	if got != expected {
+		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
+	}
+}
+
+func TestDeprecatedRangeQuery(t *testing.T) {
+	q := NewRangeQuery("postDate").
+		From("2010-03-01").
+		To("2010-04-01").
+		Boost(3).
+		Relation("within")
+	q = q.QueryName("my_query")
+	src, err := q.Source()
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, err := json.Marshal(src)
+	if err != nil {
+		t.Fatalf("marshaling to JSON failed: %v", err)
+	}
+	got := string(data)
+	expected := `{"range":{"_name":"my_query","postDate":{"boost":3,"gte":"2010-03-01","relation":"within","gte":"2010-04-01"}}}`
+	if got != expected {
+		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
+	}
+}
+
+func TestDeprecatedRangeQueryWithTimeZone(t *testing.T) {
+	q := NewRangeQuery("born").
+		Gte("2012-01-01").
+		Lte("now").
+		TimeZone("+1:00")
+	src, err := q.Source()
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, err := json.Marshal(src)
+	if err != nil {
+		t.Fatalf("marshaling to JSON failed: %v", err)
+	}
+	got := string(data)
+	expected := `{"range":{"born":{"gte":"2012-01-01","time_zone":"+1:00","lte":"now"}}}`
+	if got != expected {
+		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
+	}
+}
+
+func TestDeprecatedRangeQueryWithFormat(t *testing.T) {
+	q := NewRangeQuery("born").
+		Gte("2012/01/01").
+		Lte("now").
+		Format("yyyy/MM/dd")
+	src, err := q.Source()
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, err := json.Marshal(src)
+	if err != nil {
+		t.Fatalf("marshaling to JSON failed: %v", err)
+	}
+	got := string(data)
+	expected := `{"range":{"born":{"format":"yyyy/MM/dd","gte":"2012/01/01","lte":"now"}}}`
 	if got != expected {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
